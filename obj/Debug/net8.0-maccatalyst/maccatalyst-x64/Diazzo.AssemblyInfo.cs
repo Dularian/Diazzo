cc526b0c76f3da7bccb7c0f52e79fd005838fe08
Dularian <63817511+Dularian@users.noreply.github.com>
2024-07-12 01:54:46 -0400

    INITIALCOMMIT

--- obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Diazzo.AssemblyInfo.cs
+++ obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Diazzo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Diazzo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+30961b32f84dd818f5c9615d36b23d57b69dfc26")]
 [assembly: System.Reflection.AssemblyProductAttribute("Diazzo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Diazzo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
