b27e0777f0777e9f3568e79a095ce6abda9cca0b
Dularian <63817511+Dularian@users.noreply.github.com>
2024-07-12 01:50:59 -0400

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Diazzo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
